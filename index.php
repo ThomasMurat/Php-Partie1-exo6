148a7c7bb16be988fa5394eeabd4861f94536b32
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 15:00:08 +0200

    up exo6

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exo6</title>
+    </head>
+    <body>
+    <?php 
+        $name = 'Thomas';
+        $phrase = 'Bonjour ' . $name . ', comment vas tu ?';
+    ?>
+    <p><?php echo $phrase ;?></p>
+    </body>
+</html>
